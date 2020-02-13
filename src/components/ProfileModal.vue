e03eb0b0a58361684446a0239e1fe8ab3919a134
Dan Hassan <dh@blockades.org>
2020-02-13 14:57:50 +1100

    update name in modal

--- src/components/ProfileModal.vue
+++ src/components/ProfileModal.vue
@@ -1,3 +1,21 @@
 <template>
-<div >ProfileModal</div>
+<form @submit.prevent="onSubmit">
+  <p>Update name</p>
+  <input type="text" placeholder="My name" v-model="name" />
+  <button type="submit">Submit</button>
+</form>
 </template>
+
+<script>
+export default {
+  name: 'profile-modal',
+  data: () => ({
+    name: '',
+  }),
+  methods: {
+    onSubmit() {
+      this.$store.dispatch('updateName', this.name)
+    }
+  }
+}
+</script>
