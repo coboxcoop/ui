49343ccf8cbbcceccba8f5887f12ce70bd4c37a7
Matt Lenz <matt@mattlenz.com>
2020-02-18 11:13:03 +1100

    Wrapper and onboarding style tweaks

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,18 @@ export default {
 
 <style lang="scss" scoped>
 #app {
-  padding: 1rem;
+  padding: 1.6rem;
   display: flex;
   flex-direction: column;
   background: white;
   margin: auto;
   width: 100%;
-  max-width: 38rem;
+  height: 100%;
+  flex: 1;
+  max-width: 34rem;
+  max-height: 52rem;
+  border-radius: 4px;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.15);
 }
 </style>
 
--- src/views/Onboarding.vue
+++ src/views/Onboarding.vue
@@ -1,12 +1,25 @@
 <template>
-<div>
+<div class="container">
   <p>Welcome to CoBox. How do you want to be known to your peers?</p>
   <form @submit.prevent="onSubmitName">
     <input v-model="name" type="text" placeholder="My name" />
     <button type="submit">Ok</button>
   </form>
+
+  <div class="bottom">Restore identity</div>
 </div>
 </template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+.bottom {
+  margin-top: auto;
+}
+</style>
 
 <script>
 export default {
