68b28f24fff40b842252fea27132da827e5b1d86
Dan Hassan <dh@blockades.org>
2020-02-20 22:02:42 +1100

    wip starting filesystem locator selection

--- src/views/BackupView.vue
+++ src/views/BackupView.vue
@@ -1,6 +1,14 @@
 <template>
 <SubPage>
   <template v-slot:header>Backup</template>
+  <div class="page">
+    <p>Where would you like to save your account to?</p>
+
+    <form @submit.prevent="onSubmit">
+      <input type="text" placeholder="filesystem location" />
+      <button type="submit">Ok</button>
+    </form>
+  </div>
 </SubPage>
 </template>
 
@@ -10,6 +18,17 @@ import SubPage from '@/components/SubPage.vue'
 export default {
   components: {
     SubPage
+  },
+  methods: {
+    async onSubmit() {
+      try {
+        await this.$store.dispatch('groups/create', this.groupName)
+        this.groupName = ''
+        this.$router.replace({name: 'groups'})
+      } catch(e) {
+        this.$store.dispatch('error/handle', e)
+      }
+    }
   }
 }
 </script>
