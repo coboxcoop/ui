8c17f1e108131a6a429bce46a91c2f7771be09fa
Dan Hassan <dh@blockades.org>
2020-02-20 21:16:47 +1100

    adding menu item in index for backup

--- src/router/index.js
+++ src/router/index.js
@@ -11,20 +11,22 @@ import DevicesView from '@/views/admin/DevicesView.vue'
 import DevicesInitView from '@/views/admin/DevicesInitView.vue'
 import DevicesJoinView from '@/views/admin/DevicesJoinView.vue'
 import DevicesSetupView from '@/views/admin/DevicesSetupView.vue'
+import BackupView from '@/views/BackupView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {path: '/',                    name: 'home',                 component: HomeView},
-  {path: '/network',             name: 'network',              component: NetworkView},
-  {path: '/groups',              name: 'groups',               component: GroupsView},
-  {path: '/groups/init',         name: 'groups-init',          component: GroupsInitView},
-  {path: '/groups/join',         name: 'groups-join',          component: GroupsJoinView},
-  {path: '/groups/create',       name: 'groups-create',        component: GroupsCreateView},
+  {path: '/',                    name: 'home',                component: HomeView},
+  {path: '/network',             name: 'network',             component: NetworkView},
+  {path: '/groups',              name: 'groups',              component: GroupsView},
+  {path: '/groups/init',         name: 'groups-init',         component: GroupsInitView},
+  {path: '/groups/join',         name: 'groups-join',         component: GroupsJoinView},
+  {path: '/groups/create',       name: 'groups-create',       component: GroupsCreateView},
   {path: '/admin/devices',       name: 'admin-devices',       component: DevicesView},
   {path: '/admin/devices/init',  name: 'admin-devices-init',  component: DevicesInitView},
   {path: '/admin/devices/join',  name: 'admin-devices-join',  component: DevicesJoinView},
-  {path: '/admin/devices/setup', name: 'admin-devices-setup', component: DevicesSetupView}
+  {path: '/admin/devices/setup', name: 'admin-devices-setup', component: DevicesSetupView},
+  {path: '/backup',              name: 'backup',              component: BackupView}
 ]
 
 export default new VueRouter({
