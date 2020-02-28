3b34f5ddbee2bb20edaaf20c133b98f7b3a3e943
Dan Hassan <dh@blockades.org>
2020-02-28 23:11:18 +1100

    wip: devices store connection to websockets

--- src/store/admin/devices.js
+++ src/store/admin/devices.js
@@ -1,22 +1,32 @@
-export default api => ({
+export default ({api, events}) => ({
   namespaced: true,
   state: {
     data: []
   },
   actions: {
-    async fetch({commit}) {
+    async subscribe() {
+      events.on('event', event => {
+        console.warn(event)
+      })
+    },
+    async fetch({commit, dispatch}) {
       const {data} = await api.get('/admin/devices')
       commit('receiveData', data)
     },
     async setup({dispatch}, name) {
       const {data: {address}} = await api.post('/admin/devices', {name})
-      dispatch('join', {address, name})
+      // dispatch('join', {address, name})
       await dispatch('fetch')
+      await dispatch('connect', address)
     },
     async join({dispatch}, deviceInviteKey) {
       const [address, encryptionKey, name] = deviceInviteKey.split(':')
       await api.post('/admin/devices', {name, encryptionKey, address})
       await dispatch('fetch')
+    },
+    async connect({}, address) {
+      const {data} = await api.post(`/admin/devices${address}/connections`, {address})
+      console.warn(data)
     }
   },
   mutations: {
