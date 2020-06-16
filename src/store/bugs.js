7e2f2d2f08fa14daf6696d88ffcd0c9b9724ec9f
Matt Lenz <matt@mattlenz.com>
2020-06-16 11:49:08 +1000

    Update bug report data

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -4,13 +4,18 @@ export default ({api, events}) => ({
   namespaced: true,
   state: {},
   actions: {
-    async submit() {
+    async submit({rootState: {system}}) {
       const bugsUrl = process.env.VUE_APP_BUGS_URL
 
-      let formData = new FormData()
-      formData.append('description', 'Some data from the CoBox app, a stacktrace, versions etc')
+      let fd = new FormData()
 
-      const {data} = await axios.post(bugsUrl, formData, {
+      fd.append('stacktrace', 'stack trace here')
+ 
+      if(system.info) fd.append('server_version', system.info.version)
+      fd.append('server_status', system.info ? 'online' : 'offline')
+      fd.append('ui_version', process.env.VUE_APP_VERSION)
+
+      const {data} = await axios.post(bugsUrl, fd, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
@@ -18,6 +23,8 @@ export default ({api, events}) => ({
 
       if(data.bugId) {
         window.open(`${bugsUrl}/${data.bugId}`)
+      } else {
+        console.warn('Bug report could not be created')
       }
     }
   },
