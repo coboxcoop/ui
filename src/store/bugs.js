e0f36708e5bf7b014da051eaee12ef17f19d476b
Matt Lenz <matt@mattlenz.com>
2020-06-16 11:27:07 +1000

    Submit some data to the bugs endpoint, proof of concept

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,9 +1,24 @@
+import axios from 'axios'
+
 export default ({api, events}) => ({
   namespaced: true,
   state: {},
   actions: {
-    submit() {
-      window.open(process.env.VUE_APP_BUGS_URL)
+    async submit() {
+      const bugsUrl = process.env.VUE_APP_BUGS_URL
+
+      let formData = new FormData()
+      formData.append('description', 'Some data from the CoBox app, a stacktrace, versions etc')
+
+      const {data} = await axios.post(bugsUrl, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+
+      if(data.bugId) {
+        window.open(`${bugsUrl}/${data.bugId}`)
+      }
     }
   },
   mutations: {}
