3d5485c1e09e58073e204cc51f43ced90220ec8a
Matt Lenz <matt@mattlenz.com>
2020-06-22 15:54:27 +1000

    Add bugsnag

--- src/main.js
+++ src/main.js
@@ -4,8 +4,18 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import filters from './filters'
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginVue from '@bugsnag/plugin-vue'
 
 Vue.config.productionTip = false
+
+Bugsnag.start({
+  apiKey: '2beb08d6620bd27dc040fe44a08c78f2',
+  plugins: [new BugsnagPluginVue()]
+})
+
+Bugsnag.getPlugin('vue')
+  .installVueErrorHandler(Vue)
 
 new Vue({
   router,
