0278568baa587c6b9e1479cbdbcde5a144e38f7c
Matt Lenz <matt@mattlenz.com>
2020-07-09 15:06:21 +1000

    Provide beta tester email to bugsnag

--- src/bugsnag.js
+++ src/bugsnag.js
@@ -10,7 +10,10 @@ if(settings.betaTester && process.env.VUE_APP_BUGSNAG_API_KEY) {
   Bugsnag.start({
     apiKey: process.env.VUE_APP_BUGSNAG_API_KEY,
     plugins: [new BugsnagPluginVue()],
-    appVersion: process.env.VUE_APP_VERSION
+    appVersion: process.env.VUE_APP_VERSION,
+    metadata: {
+      email: settings.email
+    }
   })
 
   Bugsnag.getPlugin('vue')
