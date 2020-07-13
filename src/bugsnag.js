eb01e8933a008a45b968e7ed628b15037c65a149
Matt Lenz <matt@mattlenz.com>
2020-07-13 13:57:53 +1000

    Don't fetch server log if betaTester is not enabled

--- src/bugsnag.js
+++ src/bugsnag.js
@@ -1,13 +1,11 @@
 import Bugsnag from '@bugsnag/js'
 import BugsnagPluginVue from '@bugsnag/plugin-vue'
 import Vue from 'vue'
-
 import {retrieveState} from '@/store/settings'
 
 const settings = retrieveState()
 
 if(settings.betaTester && process.env.VUE_APP_BUGSNAG_API_KEY) {
-  console.log('starting bugsnag')
   Bugsnag.start({
     apiKey: process.env.VUE_APP_BUGSNAG_API_KEY,
     plugins: [new BugsnagPluginVue()],
--- src/store/system.js
+++ src/store/system.js
@@ -1,5 +1,6 @@
 const LOGS_FETCH_INTERVAL = 10 * 1000 // 10s
 import Bugsnag from '@/bugsnag'
+import {retrieveState} from '@/store/settings'
 
 export default ({api, events}) => ({
   namespaced: true,
@@ -17,9 +18,12 @@ export default ({api, events}) => ({
       }
     },
     async fetchLogs({dispatch}) {
-      let {data} = await api.get('/system/logs')
-      Bugsnag.addMetadata('server_logs', data)
-      setTimeout(() => dispatch('fetchLogs'), LOGS_FETCH_INTERVAL)
+      const settings = retrieveState()
+      if(settings && settings.betaTester) {
+        let {data} = await api.get('/system/logs')
+        Bugsnag.addMetadata('server_logs', data)
+        setTimeout(() => dispatch('fetchLogs'), LOGS_FETCH_INTERVAL)
+      }
     }
   },
   mutations: {
