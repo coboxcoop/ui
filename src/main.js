fc2991e0c427b412e5479840484f35e8a7508aeb
Matt Lenz <matt@mattlenz.com>
2020-07-15 16:04:02 +1000

    Add vue shortkey

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueShortkey from 'vue-shortkey'
 import './filters'
 import './bugsnag'
 
 Vue.config.productionTip = false
+
+Vue.use(VueShortkey)
 
 new Vue({
   router,
