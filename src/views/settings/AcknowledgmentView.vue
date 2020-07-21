f540b656f6cf1ca97bfb9bb8ae4867d14baf0335
Dan Hassan <dh@magmacollective.org>
2020-07-21 22:18:13 +1000

    update content

--- src/views/settings/AcknowledgmentView.vue
+++ src/views/settings/AcknowledgmentView.vue
@@ -1,8 +1,8 @@
 <template>
 <Screen :back="{name: 'settings'}">
   <Modal :show="showShortKeyInfo" @close="showShortKeyInfo = false">
-    <p> This interface has been built on Boon Wurrung and Wurundjeri lands of the Eastern Kulin Nations, under illegal occupation by the settler state known as Australia.</p>
-    <p> Their sovereignty was never ceeded and resistance has been fertile and ongoing.</p>
+    <p> This interface has been built on the lands of the Boon Wurrung and Wurundjeri peoples of the Eastern Kulin Nations, under ongoing illegal occupation by the settler state known as Australia.</p>
+    <p> Their sovereignty was never ceeded and resistance has been fertile and ongoing since 1788.</p>
     <p> We pay our respects to their leaders and elders, past and present.<p>
     <p>1% of funds used to build this interface have been passed to <a href="https://paytherent.net.au/" target="_blank">Pay The Rent</a> and <a href="https://www.indigitek.org.au" target="_blank">Indigitek</a>.</p>
     <p> Whose land are you on? Do you know? Do you acknowledge their right to it? If you live on colonised - stolen - land it is important you realise this.</p>
