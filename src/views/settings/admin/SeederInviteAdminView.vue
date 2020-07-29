4317e594a18b70641a5fb573bc89079edb3280ee
Dan Hassan <dh@magmacollective.org>
2020-07-30 05:45:24 +1000

    update seeder actions with info notes

--- src/views/settings/admin/SeederInviteAdminView.vue
+++ src/views/settings/admin/SeederInviteAdminView.vue
@@ -1,11 +1,17 @@
 <template>
 <Screen :back="{name: 'seeder', params: {address: $route.params.address}}" v-shortkey="['ctrl', 'p']" @shortkey.native="navigate({name: 'profile'})">
+  <Modal :show="showInfo" @close="showInfo = false">
+    <p>This invite will be encrypted to your friends CoBox key, so only they can use it. It's safe to pass to them via an insecure messaging application.</p>
+  </Modal>
+
   <div v-if="inviteCode">
     <p>Please send the following invite code to your friend. This will make them an admin of this seeder.</p>
     <CopyKey :value="inviteCode" />
   </div>
   <div v-else>
-    <p>To invite your friend to be an admin of this seeder, please provide their CoBox key.</p>
+    <div class="info">
+      <p>To invite your friend to be an admin of this seeder, please provide their CoBox key. <img @click="showInfo = true" v-shortkey="['ctrl', 'i']" @shortkey="navigate(showInfo = true)" src="@/assets/images/icons/info.svg" /></p>
+    </div>
 
     <form @submit.prevent="onSubmitInvite">
       <input class="has-ok-button" type="text" placeholder="Friends CoBox Key" v-model="publicKey" />
@@ -24,18 +30,35 @@
   border-top: 1px solid var(--fg);
   padding-top: 0.6rem;
 }
+.info {
+  display: flex;
+  align-items: center;
+  p {
+    flex: 1;
+    img {
+      margin-bottom: -0.1em;
+      cursor: pointer;
+      html.dark & {
+        filter: invert(1);
+      }
+    }
+  }
+}
 </style>
 
 <script>
 import Screen from '@/components/Screen.vue'
 import CopyKey from '@/components/CopyKey.vue'
+import Modal        from '@/components/Modal.vue'
 
 export default {
   components: {
     Screen,
-    CopyKey
+    CopyKey,
+    Modal
   },
   data: () => ({
+    showInfo: false,
     inviteCode: '',
     publicKey: '',
     address: '',
--- src/views/settings/admin/SeederSeedFolderView.vue
+++ src/views/settings/admin/SeederSeedFolderView.vue
@@ -1,5 +1,9 @@
 <template>
   <Screen :back="{name: 'seeder', params: {address: seeder.address}}" v-if="seeder">
+  <Modal :show="showNameInfo" @close="showNameInfo = false">
+    <p>This is the name the seed will be known as on this seeder. It can't be changed. Other seeder admins also use this name. Making this name editable will be included in a future release.</p>
+  </Modal>
+
   <template v-slot:header>
     <div class="header">
       Seeder > {{seeder.name}} > Seed folder
@@ -7,7 +11,9 @@
   </template>
 
   <br />
-  <p>Please get the CoBox address for the folder which you would like to seed.</p>
+  <div class="info">
+    <p>The peer needs to give the CoBox address for the folder which they'd like you to seed. Assign a name. <img @click="showNameInfo = true" v-shortkey="['ctrl', 'i']" @shortkey="navigate(showNameInfo = true)" src="@/assets/images/icons/info.svg" /></p>
+  </div>
   <form @submit.prevent="onSubmitAddReplicate">
     <input type="text" placeholder="Folder address" v-model="address">
     <input class="has-ok-button" type="text" placeholder="Name" v-model="name">
@@ -24,18 +30,35 @@
 .space-icon {
   margin-right: 0.3rem;
 }
+.info {
+  display: flex;
+  align-items: center;
+  p {
+    flex: 1;
+    img {
+      margin-bottom: -0.1em;
+      cursor: pointer;
+      html.dark & {
+        filter: invert(1);
+      }
+    }
+  }
+}
 </style>
 
 <script>
 import Screen    from '@/components/Screen.vue'
 import NavList   from '@/components/NavList.vue'
+import Modal        from '@/components/Modal.vue'
 
 export default {
   components: {
     Screen,
-    NavList
+    NavList,
+    Modal
   },
   data: () => ({
+    showNameInfo: false,
     address: '',
     name: ''
   }),
