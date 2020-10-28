cfb2634a2787ebf0bcee6cc55c542ffd90702b6f
Dan Hassan <dh@magmacollective.org>
2020-10-28 20:30:24 +1100

    wip: figuring out nested v-for

--- src/views/settings/ContactsView.vue
+++ src/views/settings/ContactsView.vue
@@ -6,64 +6,45 @@
   <NavList>
     <RouterLink v-for="space in spaces" :key="space.address" class="space" :to="{name: 'space', params: {address: space.address}}" v-shortkey="['ctrl', 'enter' ]" @shortkey.native="navigate({name: 'space', params: {address: space.address}})">
       <div>{{space.name}}</div>
-      <div>Contacts list here.</div>
+      <div>Contacts list here. {{space.address}}</div>
+      <!-- 1. nested query? 
+           2. for each space in spaces 
+           3. show peers in each space
+           4. getting this.space is not known-->
+      <NavList>
+        <div v-for="peer in peers" :key="peer.publicKey">
+          <UserIcon :address="peer.data.author" /> {{peer.data.content.name}}
+          <CopyKey :value="peer.data.author" />
+        </div>
+      </NavList>
     </RouterLink>
   </NavList>
-
-  <!-- <h2>{{peerCountString}}</h2> -->
-
-  <!-- <NavList> -->
-  <!--   <div v&#45;for="peer in peers" :key="peer.publicKey"> -->
-  <!--     <UserIcon :address="peer.data.author" /> {{peer.data.content.name}} -->
-  <!--     <CopyKey :value="peer.data.author" /> -->
-  <!--   </div> -->
-  <!-- </NavList> -->
-
-  <br />
-  
 </Screen>
 </template>
-
-<style scoped lang="scss">
-.switch {
-  display: flex;
-  align-items: center;
-  label {
-    flex: 1;
-    img {
-      margin-bottom: -0.1em;
-      cursor: pointer;
-      html.dark & {
-        filter: invert(1);
-      }
-    }
-  }
-}
-</style>
 
 <script>
 import Screen       from '@/components/Screen.vue'
 import NavList      from '@/components/NavList.vue'
 import UserIcon     from '@/components/UserIcon.vue'
+import CopyKey      from '@/components/CopyKey.vue'
 
 export default {
   components: {
     Screen,
     NavList,
-    UserIcon
+    UserIcon,
+    CopyKey
   },
   computed: {
     spaces() {
       return this.$store.state.spaces.data
+    },
+    space() {
+      return this.$store.getters['spaces/single'](this.$route.params.address)
+    },
+    peers() {
+      return this.$store.getters['spaces/peers'](this.space.address)
     }
-    // peerCountString() {
-    //   const count = this.$store.getters['spaces/peerCount'](this.space.address)
-    //
-    //   return `${count} friend${count != 1 ? 's' : ''}`
-    // },
-    // peers() {
-    //   return this.$store.getters['spaces/peers'](this.space.address)
-    // }
   },
   methods: {
     navigate(to) {
