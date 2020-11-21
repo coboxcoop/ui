551aed0790715803db0002eba434731832b82652
Dan Hassan <dh@magmacollective.org>
2020-11-21 19:59:40 +1100

    wip: add testing for contacts view feature

---
+++ tests/unit/Contacts.spec.js
@@ -0,0 +1,28 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import VueRouter from "vue-router"
+import ContactsView from '@/views/settings/ContactsView.vue'
+import ContactsFoldersView from '@/views/settings/ContactsFoldersView.vue'
+import routes from '@/router/index.js'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+
+jest.mock("@/views/settings/ContactsFoldersView", () => ({
+  name: "ContactsFoldersView",
+  render: h => h("div")
+}))
+
+describe('ContactsView.vue', () => {
+  it('renders a child component via routing', async () => {
+    const router = new VueRouter({ routes })
+    const wrapper = mount(ContactsView, {
+      localVue,
+      router
+    })
+
+    router.push("/contacts-folders-view")
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.findComponent(ContactsFoldersView).exists()).toBe(true)
+  })
+})
