ed65448007871c4d361b3b1f68cdb19abdc3252d
Dan Hassan <dh@magmacollective.org>
2020-11-25 08:36:07 +1100

    add vue-select dependency for autosuggest

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import VueShortkey from 'vue-shortkey'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue          from 'vue'
+import VueShortkey  from 'vue-shortkey'
+import vSelect      from 'vue-select'
+import App          from './App.vue'
+import router       from './router'
+import store        from './store'
 import './filters'
 import './bugsnag'
 
 Vue.config.productionTip = false
 
 Vue.use(VueShortkey)
+Vue.use(vSelect)
 
 new Vue({
   router,
