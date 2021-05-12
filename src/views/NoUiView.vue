e4b35aeb51d0e1246a2f55e3b164bcacd69709cc
Kieran Gibb <kg@magmacollective.org>
2021-05-12 11:59:07 +0100

    add additional option of setting ui: false in coboxrc

--- src/views/NoUiView.vue
+++ src/views/NoUiView.vue
@@ -9,8 +9,14 @@
     Visit the <br /><a href="https://gitlab.com/coboxcoop/ui">source code</a> for instructions.
   </p>
   <p>
-    Alternatively to stop this page<br />appearing,
-    you can run the<br />server in headless mode:
+    Alternatively, to stop this page<br />appearing,
+    you can add
+  </p>
+  <p>
+  <pre>ui: false</pre>
+  </p>
+  <p>
+    to your <pre>.coboxrc</pre>, or run<br />the server in headless mode
   </p>
   <p>
     <pre>cobox start --no-ui</pre>
@@ -43,6 +49,10 @@ export default {
   }
   a {
     text-decoration: underline;
+  }
+  pre {
+    display: inline;
+    margin: 0 4px 0 4px;
   }
 }
 </style>
