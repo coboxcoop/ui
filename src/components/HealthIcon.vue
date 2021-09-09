54fde6afb50e26b39867730087ae2efd16865916
Kieran Gibb <kg@magmacollective.org>
2021-09-09 14:20:50 +0100

    quick sketch of health view

---
+++ src/components/HealthIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="health-icon">
+    <!-- TODO: fix the if else -->
+    <div v-if="true">
+      <img style="margin: 1rem 1rem 0 0; height: 6rem;" src="@/assets/images/icons/health-green.png" alt="">
+    </div>
+    <div v-else>
+      <!-- TODO: create a warning icon! -->
+      <img style="margin: 1rem 1rem 0 0; height: 6rem;" src="@/assets/images/icons/health-red.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+  }
+}
+</script>
+
