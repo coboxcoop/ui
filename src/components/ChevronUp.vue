2ef7b94473b31635510ce3d85239d48e9a34516e
aeluteia <aeluteia@riseup.net>
2021-10-01 14:38:21 +0100

    Create chevron components

---
+++ src/components/ChevronUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <chevron-up-icon class="chevron-up" ></chevron-up-icon>
+</template>
+
+<script>
+import { ChevronUpIcon } from 'vue-feather-icons'
+
+export default {
+  components: {
+    ChevronUpIcon
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.chevron-up {
+    color: white;
+}
+</style>
